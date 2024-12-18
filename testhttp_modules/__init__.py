fb8135f81c804f419523c1c3583f2f4763ccdfeb
owne-syn <owen.loisel@synacktiv.com>
2024-12-18 15:08:27 +0100

    Add files via upload

---
+++ testhttp_modules/__init__.py
@@ -0,0 +1,6 @@
+from sekoia_automation.module import Module
+from testhttp_modules.models import testhttp_modulesConfiguration
+
+
+class testhttp_modules(Module):
+    configuration: testhttp_modulesConfiguration
