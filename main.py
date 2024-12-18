7efdface967f01a0959a1eb1c3f6c8930e2f0799
owne-syn <owen.loisel@synacktiv.com>
2024-12-18 15:05:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from synacktiv-test_modules import Synacktiv-TestModule
+from testhttp_modules import testhttp_modules
+
+from testhttp_modules.trigger_new_entries import NewEntriesTrigger
+from testhttp_modules.action_request import Request
+
 
 if __name__ == "__main__":
-    module = Synacktiv-TestModule()
+    module = testhttp_modules()
+    module.register(NewEntriesTrigger, "NewEntriesTrigger")
+    module.register(Request, "Request")
     module.run()
